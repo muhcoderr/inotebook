ef6e3c505ddae3e8dc083dc5afce091839718de0
Muhammad Tariq <muhammad4948@gmail.com>
2023-11-12 16:47:43 +0500

    Thunder Clients Collections to Manage Requests
    Thunder Clients Collections to Manage Requests
    
    Collections Folder Export

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -15,25 +15,33 @@ router.post(
     }),
   ],
   async (req, res) => {
-      // If there are errors, return Bad request and the errors
+    // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    
-    User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({
-          error: "Please enter a unique value for email",
-          message: err.message,
-        });
-      });
+
+    //check whether the user with this email exists already
+    try {
+
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "Sorry a user with this email already exists" })
+
+      }
+      // create a new users
+      user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      })
+      res.json(user)
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some Error occured");
+
+    }
+
   }
 );
 
