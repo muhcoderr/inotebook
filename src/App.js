5a9748068f93de3ebb7a109c65cb2db81c15a5f0
Muhammad Tariq <muhammad4948@gmail.com>
2023-11-16 23:15:15 +0500

    Create Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
